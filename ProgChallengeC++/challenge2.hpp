e7a6c46485fff9ae311b07f5a15b3aab7c84aed1
Jorge Jones <iamthegrandfinale@yahoo.com>
2019-01-13 17:03:54 -0600

    Initial Commit

---
+++ ProgChallengeC++/challenge2.hpp
@@ -0,0 +1,14 @@
+//
+//  challenge2.hpp
+//  ProgChallengeC++
+//
+//  Created by home on 1/7/19.
+//  Copyright © 2019 jorgemichaeljones. All rights reserved.
+//
+
+#ifndef challenge2_hpp
+#define challenge2_hpp
+
+#include <stdio.h>
+
+#endif /* challenge2_hpp */
